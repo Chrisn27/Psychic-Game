718c4583cbf510f91e640342338dd2975a4f35d9
chrisn27 <chris_nguyen27@yahoo.com>
2017-04-04 19:35:35 -0700

    updated game.js file, cleaner and such

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-	//array list, characters a-z
+//array list, characters a-z
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //wins, losses and lives remaining - starting at 10
 var wins = 0;
@@ -6,25 +6,24 @@ var losses = 0;
 var guessesLeft = 10;
 var userGuesses = [];
 
-      var reset = function() {
+    var reset = function() {
 
   	  	totalGuesses = 10;
   	  	guessesLeft = 10;
   	  	userGuesses = [];
 
-	  	}
+	  }
 
 	    // Determines which key was pressed by user
-      document.onkeyup = function(event) {
+document.onkeyup = function(event) {
 
       var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-      var choices = "abcdefghijklmnopqrstuvwxyz";
 
+      // pushes userGuess to the userGuesses array
       userGuesses.push(userGuess);
 
       // Psychic computer choice or just a randomly selected index
       var computerGuess = options[Math.floor(Math.random() * options.length)];
-      //console.log(computerGuess);
 
       // sanity checking 
       // console.log("------------1-------------");
@@ -36,35 +35,39 @@ var userGuesses = [];
       // console.log("-------------------------");
       // console.log(event.keyCode)
 
+      // win condition
       if (userGuess === computerGuess) {
             wins++;
-            alert("You win!  Feel good about yourself.");
+            alert("You win!  You are super gifted.");
             reset();
       }
 
+      // lose a guess if you're not a psychic
       else if (userGuess !== computerGuess) {
             guessesLeft--;
       }
 
+      // resets game if you try to pick anything outside of the english alphabet
       if (event.keyCode < 65 || event.keyCode > 90) {
             alert("Please only use A-Z.  Resetting game");
             reset();
       }
 
+      // conditions for losing.
       if (guessesLeft <= 0) {
             losses++;
-            alert("You lose!  Feel bad about yourself.");
+            alert("You lose!  You are not gifted..");
             reset();
       }
 
-          var html = "<h3>Guess what letter I'm thinking!  A - Z</h3>" +
-          "<p>wins: " + wins + "</p>" +
-          "<p>losses: " + losses + "</p>" +
-          "<p>lives remaining: " + guessesLeft + "</p>" +
-          "<p>Guesses so far: "+ userGuesses.join(', ') + "</p>";
+                  //overwrites div target html with the above text
+      var html =  "<h3>Guess what letter I'm thinking!  A - Z</h3>" +
+                  "<p>wins: " + wins + "</p>" +
+                  "<p>losses: " + losses + "</p>" +
+                  "<p>lives remaining: " + guessesLeft + "</p>" +
+                  "<p>Guesses so far: "+ userGuesses.join(', ') + "</p>";
 
-          //overwrites div target html with the above text
-          document.querySelector("#game_stats").innerHTML = html;
+                  document.querySelector("#game_stats").innerHTML = html;
 
 };
 
